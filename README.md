78b9f9d2513323e0ffb5581076a8b0e1dbab014a
milankyncl <milan.kyncl@icloud.com>
2018-04-17 22:46:25 +0200

    initialization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# flex-grid-framework
+# Flex Grid Framework
+
 Super minimalistic CSS flex grid framework for easy manipulation with friendly-responsive websites.
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+/**
+ *
+ */
+
+gulp.task('start', function () {
+
+    gulp.watch('src/flex-grid.scss', [ 'process_scss' ]);
+});
+
+gulp.task('process_scss', function () {
+
+    var plugins = [
+        autoprefixer({browsers: ['last 5 version']}),
+        cssnano()
+    ];
+
+    return gulp.src([
+                './src/flex-grid.scss'
+            ])
+            .on('error', onError)
+            .pipe(sass())
+            .on('error', onError)
+            .pipe(postcss(plugins))
+            .on('error', onError)
+            .pipe(cssmin())
+            .on('error', onError)
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest('./assets/build/css'));
+});
+
+function onError(err) {
+
+    console.log(err);
+
+    this.emit('end');
+}
